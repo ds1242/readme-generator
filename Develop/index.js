e3b0a08ce8189210f5f2227407c566ec66628fd3
dshaw1242 <david.shaw1242@gmail.com>
2021-05-06 19:12:11 -0600

    prompt user with questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -93,7 +94,10 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
+    
